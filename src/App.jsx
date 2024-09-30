d893f6aae62fa9c9fb947edfd7f29ab0412f6f2d
Maksym <b.orlov7709@gmail.com>
2024-09-30 18:42:26 +0300

    upd vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
-import './scss/style.css'
 import Buy from './pages/Buy/Buy.jsx'
 import SoftWare from './pages/SoftWare/SoftWare.jsx'
-// import Recleaner from './pages/Recleaner/Recleaner.jsx'
+import './scss/style.css'
 
 const App = () => {
 	return (
@@ -13,7 +12,6 @@ const App = () => {
 				<Route path={'/'} element={<Home/>}></Route>
 				<Route path={'/buy'} element={<Buy/>}></Route>
 				<Route path={'/software'} element={<SoftWare/>}></Route>
-				{/*<Route path={'/recleaner'} element={<Recleaner/>}></Route>*/}
 			</Routes>
 		</BrowserRouter>
 	)
