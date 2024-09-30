25ad31ac94a1ec889fa6ba5f39e71f88e4b2f38c
Maksym <b.orlov7709@gmail.com>
2024-09-30 19:32:28 +0300

    upd env

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,9 +2,9 @@ import { getDatabase, ref, set, get, child } from 'firebase/database';
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-	apiKey: import.meta.env.FIREBASE_API_KEY,
-	messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
-	appId: import.meta.env.FIREBASE_APP_ID,
+	apiKey: import.meta.env.VITE_API_KEY,
+	messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_APP_ID,
 	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
 	databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
 	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
 import Buy from './pages/Buy/Buy.jsx'
 import SoftWare from './pages/SoftWare/SoftWare.jsx'
-import './scss/style.css'
 import Recleaner from './pages/Recleaner/Recleaner.jsx'
+import './scss/style.css'
 
 const App = () => {
 	return (
