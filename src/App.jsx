56c8bd273df60a802bcecdbb743f21dc1186e625
Maksym <b.orlov7709@gmail.com>
2024-10-01 11:35:27 +0300

    fix problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home.jsx'
 import Buy from './pages/Buy/Buy.jsx'
 import SoftWare from './pages/SoftWare/SoftWare.jsx'
-import Recleaner from './pages/Recleaner/Recleaner.jsx'
 import './scss/style.css'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Recleaner from './pages/Recleaner/Recleaner.jsx'
 
 const App = () => {
 	return (
-		<Router basename='/clean-tweaking'>
+		<BrowserRouter>
 			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/buy' element={<Buy />} />
-				<Route path='/software' element={<SoftWare />} />
-				<Route path='/recleaner' element={<Recleaner />} />
+				<Route path='/' element={<Home/>}></Route>
+				<Route path='/buy' element={<Buy/>}></Route>
+				<Route path='/software' element={<SoftWare/>}></Route>
+				<Route path='/recleaner' element={<Recleaner/>}></Route>
 			</Routes>
-		</Router>
+		</BrowserRouter>
 	)
 }
 
