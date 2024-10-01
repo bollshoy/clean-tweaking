c9eb513d0f038971995c582a2bdcbd86945c791e
Maksym <b.orlov7709@gmail.com>
2024-10-01 12:32:43 +0300

    upd app

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import React from 'react';
-import Home from './pages/Home/Home.jsx';
-import Buy from './pages/Buy/Buy.jsx';
-import SoftWare from './pages/SoftWare/SoftWare.jsx';
-import Recleaner from './pages/Recleaner/Recleaner.jsx';
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import './scss/style.scss';
+import React from 'react'
+import Home from './pages/Home/Home.jsx'
+import Buy from './pages/Buy/Buy.jsx'
+import SoftWare from './pages/SoftWare/SoftWare.jsx'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Recleaner from './pages/Recleaner/Recleaner.jsx'
+import './scss/style.css'
 
 const App = () => {
-	const location = useLocation();
-	
 	return (
-		<TransitionGroup>
-			<CSSTransition key={location.key} classNames="page" timeout={1200}>
-				<Routes location={location}>
-					<Route path='/' element={<Home />} />
-					<Route path='/buy' element={<Buy />} />
-					<Route path='/software' element={<SoftWare />} />
-					<Route path='/recleaner' element={<Recleaner />} />
-				</Routes>
-			</CSSTransition>
-		</TransitionGroup>
-	);
-};
+		<Router basename='/clean-tweaking'>
+			<Routes>
+				<Route path={'/'} element={<Home/>}></Route>
+				<Route path={'/buy'} element={<Buy/>}></Route>
+				<Route path={'/software'} element={<SoftWare/>}></Route>
+				<Route path={'/recleaner'} element={<Recleaner/>}></Route>
+			</Routes>
+		</Router>
+	)
+}
 
-const Main = () => (
-	<BrowserRouter basename={'/clean-tweaking'}>
-		<App />
-	</BrowserRouter>
-);
-
-export default Main;
+export default App
